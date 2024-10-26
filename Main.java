196a55588e73fcde5712e9facf08037d86cba2ee
Martyna0405 <kotulska.martyna1@gmail.com>
2024-10-26 12:33:37 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,147 @@
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//          System.out.println("Nasz pierwsza aplikacja");
+//
+//           int number = 7;
+//           int conditionNumber = 14;
+//
+//           if (number>conditionNumber){
+//               System.out.println("Liczba "+ number+" jest większa od "+conditionNumber);    }
+//          else if (number<conditionNumber) {
+//               System.out.println("Liczba " + number + " jest mniejsza od " + conditionNumber);  }
+//           else  {
+//              System.out.println("Liczby są równe");    }
+//
+//          if (number>conditionNumber) {
+//               System.out.println("Liczba " + number + " jest większa od " + conditionNumber);
+//                if(number == 7){//           System.out.println("Liczba jest");        }       }
+//
+//            int number;
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Podaj liczbe");
+//            number = input.nextInt();
+//            input.close();
+//
+//           System.out.println(number);
+//
+//            if (number %2 == 0) {
+//                System.out.println("Liczba jest parzysta");
+//            }
+//            else{
+//                System.out.println("Liczba jest nieparzysta");
+//            }
+//
+//            int number=10;
+//
+//            for (int i=0; i<number; i++){
+//              System.out.println(i+" Hello world");
+//           }
+//
+//           int number=10;
+//
+//           for (int i=0; i<number; i+=2){
+//              System.out.println(i+" Hello world");
+//          }
+//
+//           int number;
+//             Scanner input = new Scanner(System.in);
+//              System.out.print("Podaj liczbe");
+//               number = input.nextInt();
+//               input.close();
+//
+//           for(int a=0; a<number; a++) {
+//               for (int i = 0; i <= number; i++) {
+//                   if (i < number) {
+//                       System.out.print(i + ", ");
+//                   } else {
+//                       System.out.print(i);
+//                   }
+//               }
+//               System.out.println();
+//           }
+
+
+//        int count=0;
+//
+//        while(count<10){
+//            System.out.println(count);
+//            count++;
+//        }
+
+
+//                 boolean done= false;
+//                    while(!done){
+//              try {
+//                     int number;
+//                   Scanner input = new Scanner(System.in);
+//                   System.out.print("Podaj liczbe");
+//                   number = input.nextInt();
+//                  input.close();
+//                      for(int a=0; a<number; a++) {
+//                      for (int i = 0; i <= number; i++) {
+//                          if (i < number) {
+//                              System.out.print(i + ", ");
+//                          } else {
+//                              System.out.print(i);
+//                          }
+//                      }
+//                      System.out.println();
+//                  }
+//                      done = true;
+//              } catch(InputMismatchException exception){
+//                  System.out.println("Proszę podać liczbę całkowitą");
+//              }
+//        }
+
+
+//        ZADANIE
+//        Program do konwersji wartosci temperatury z C na farenheita
+
+//              try{
+//        System.out.println("Program do konwersji stopni C na F");
+//
+//            Scanner input = new Scanner(System.in);
+//            System.out.print("Podaj liczbe");
+//            double number = input.nextDouble();
+//            input.close();
+//             number=number*1.8;
+//             number+=32.0;
+//            System.out.print("Podane stopnie C wynoszą: " + number+ " w skali F");
+//    } catch(InputMismatchException exception){
+//                  System.out.println("Proszę podać liczbę");
+//              }
+
+
+//                ZADANIE2
+//              Wczytać od użytkownika 3 liczby całkowitą i wypisać największą oraz najmniejszą
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Podaj pierwsza liczbe");
+//        int number1 = input.nextInt();
+//
+//        System.out.print("Podaj druga liczbe");
+//        int number2 = input.nextInt();
+//
+//        System.out.print("Podaj trzecia liczbe");
+//        int number3 = input.nextInt();
+//
+//        if (number1 > number2) {
+//            if (number1 > number3) {
+//                System.out.println("Największa liczba to " + number1);
+//            }
+//        }
+//          else if (number2 > number3) {
+//                System.out.println("Największa liczba to " + number2);
+//            }
+//                else {
+//                System.out.println("Największa liczba to " + number3);
+//        }
+//        input.close();
+
+    }
+}
